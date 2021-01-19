09ecfa5c143f5e57da97e77f6defda69ad5c8384
titorivera0707 <titorivera0707@gmail.com>
2021-01-18 22:15:44 -0800

    Simplifying Month

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,23 +1,17 @@
-import React, {Component} from "react"
+import React from "react"
 import Rows from "../Rows"
 import Month from "../Month"
 
-class Calendar extends Component {
+function Calendar() {
 
-    state = {
-        months: new Date().toLocaleString('default', { month: "long" })
-    }
+    const months = new Date().toLocaleString('default', { month: "long" })
 
-    render() {
-        const months = this.state.months
-        console.log(months)
-        return(
-        <div>
-            <Month month={months}/>
-            <Rows first={"Monday"} second={"Tuesday"} third={"Wednesday"} fourth={"Thursday"} fifth={"Friday"} sixth={"Saturday"} seventh={"Sunday"}/>
-        </div>
-        )
-    }
+    return(
+    <div>
+        <Month month={months}/>
+        <Rows first={"Monday"} second={"Tuesday"} third={"Wednesday"} fourth={"Thursday"} fifth={"Friday"} sixth={"Saturday"} seventh={"Sunday"}/>
+    </div>
+    )
 }
 
 export default Calendar
